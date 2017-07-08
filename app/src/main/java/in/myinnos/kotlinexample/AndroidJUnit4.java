dca6ab508274b4499e923bffbe4a4b9c029f90c4
Sanket Devrao Selokar <sanket.selokar@gmail.com>
2017-07-08 18:29:48 -0500

    Final Commit

---
+++ app/src/main/java/in/myinnos/kotlinexample/AndroidJUnit4.java
@@ -0,0 +1,10 @@
+package in.myinnos.kotlinexample;
+
+import org.junit.runner.Runner;
+
+/**
+ * Created by Sanket on 6/23/2017.
+ */
+
+abstract class AndroidJUnit4 extends Runner {
+}
---
+++ app/src/main/java/in/myinnos/kotlinexample/InstrumentationRegistry.java
@@ -0,0 +1,13 @@
+package in.myinnos.kotlinexample;
+
+import android.content.Context;
+
+/**
+ * Created by Sanket on 6/23/2017.
+ */
+
+class InstrumentationRegistry {
+    public static Context getTargetContext() {
+        return null;
+    }
+}
